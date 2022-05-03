4862daa848430c7c9c28e320c029c86d42ec6005
Cobollatin <Cobollatin@hotmail.com>
2022-05-02 21:02:03 -0500

    Fake API

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>login prueba</div>
+  <v-container>
+    <div>login prueba</div>
+    <div>{{ message }}</div>
+  </v-container>
 </template>
 
 <script>
   export default {
     name: 'Login',
+    data () {
+      return {
+        message: '',
+      }
+    },
+    async mounted () {
+      const { text } = await (await fetch('/api/message')).json()
+      this.message = text
+    },
   }
 </script>
