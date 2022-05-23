5eb7a54b3641e7dce17f88e3ea6ddbedfa5ac539
Alimdreams <leonsahir7@gmail.com>
2022-05-22 21:29:53 -0500

    closes #15

--- src/app/modules/admin/pages/quote/quote.component.ts
+++ src/app/modules/admin/pages/quote/quote.component.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
     selector       : 'quote',
     templateUrl    : './quote.component.html',
@@ -8,10 +9,48 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 })
 export class QuoteComponent
 {
-    /**
-     * Constructor
-     */
+    providers: any[] = [
+        {
+            name  : 'Azure',
+            value : 'azure'
+        },
+        {
+            name  : 'AWS',
+            value : 'aws'
+        },
+        {
+            name  : 'Oracle',
+            value : 'oracle'
+        }
+    ];
+    services: any[] = [
+        {
+            name  : 'Virtual Machines',
+            value : 'vm'
+        },
+        {
+            name  : 'Database',
+            value : 'db'
+        },
+        {
+            name  : 'Serverless Computing',
+            value : 'sl'
+        }
+    ];
+    formProvider = new FormControl();
+    formService = new FormControl();
+    selectedProvider: any[];
+    selectedService: any[];
     constructor()
     {
     }
+    addProvider() {
+        this.selectedProvider=this.formProvider.value;
+    }
+
+    addService() {
+        this.selectedService=this.formService.value;
+    }
+
 }
+
